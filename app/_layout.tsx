8157f5ce28c6cba5dba258f1ddea6d488a7339d6
Cristian Figueredo <cristian28figueredo@gmail.com>
2025-02-20 12:23:16 -0300

    fix: update status bar and user interface style configuration

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,17 +5,14 @@ import { Slot } from "expo-router";
 import { SupabaseProvider } from "@/context/supabase-provider";
 import { registerGlobals } from "@livekit/react-native";
 import { colors } from "@/constants/colors";
-import { StatusBar } from "react-native";
+import { StatusBar } from "expo-status-bar";
 
 registerGlobals();
 
 export default function AppLayout() {
 	return (
 		<SupabaseProvider>
-			<StatusBar
-				barStyle="light-content"
-				backgroundColor={colors.dark.background}
-			/>
+			<StatusBar style="light" backgroundColor={colors.dark.background} />
 			<Slot />
 		</SupabaseProvider>
 	);
